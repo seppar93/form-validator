830dacb97cbfcaec2d41f5ca1a14298063dff9a4
Sepehr Parirokh <sepehrparirokh@gmail.com>
2020-04-19 23:57:17 -0400

    validations compleete

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Elements
 const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
@@ -18,45 +19,57 @@ function showSuccess(input) {
   formControl.className = 'form-control success';
 }
 
-function isValidEmail(email) {
+function checkEmail(input) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  // return re.test(String(input).toLowerCase());
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email is not valid');
+  }
 }
 
 function checkRequired(inputArr) {
-  inputArr.forEach(function(input){
-    if(input.value.trim() === ''){
-      
-      showError(input, `${getFieldName(input)} is required`)
+  inputArr.forEach(function (input) {
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`);
     } else {
       showSuccess(input);
     }
-    
-
-  })
+  });
 }
+
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(input, `${getFieldName(input)} must be at lease ${min}`);
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less than ${max} characters`
+    );
+  } else {
+    showSuccess(input);
+  }
+}
+
 function getFieldName(input) {
-  return input.id.charAt(0).toUpperCase() + input.id.slice(1)
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+function checkPasswordsMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, 'Passwords do not match');
+  }
 }
 // Event Listeners
 
 form.addEventListener('submit', function (event) {
   event.preventDefault();
-  checkRequired([username, email, password, password2])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 8, 25);
+  checkEmail(email);
+  checkPasswordsMatch(password, password2);
 
   // if (username.value === '') {
   //   showError(username, 'username is required');
